f620a73777e62fbef1a226e52c898d48a5944a21
yanwei865184884_0aP <yanwei865184884>
2021-08-31 19:51:07 +0800

    commiT

---
+++ demo/src/main/java/com/example/yanwei/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.yanwei;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author yanwei
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
